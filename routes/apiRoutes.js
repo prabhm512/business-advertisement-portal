25c324d4cd8418b4698d95a5b86952e7cbb1501a
Felipe Ferreira <felipesqf@gmail.com>
2020-11-19 21:12:33 +1100

    posting route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,4 +8,20 @@ module.exports = function(app) {
       res.json(ads);
     });
   });
+  //post the add
+  app.post("/api/advertisements", (req, res) => {
+    db.Advertisement.create({
+      prodName: req.body.name,
+      prodImg: req.body.image,
+      marketPrice: req.body.marketPrice,
+      offeredPrice: req.body.offeredPrice,
+      description: req.body.description
+    })
+      .then(ads => {
+        res.json(ads);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 };
