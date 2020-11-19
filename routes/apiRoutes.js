329679dbabb368f4d7826406343c5a6e148853e8
Felipe Ferreira <felipesqf@gmail.com>
2020-11-19 20:56:31 +1100

    changing the get rout to include business db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,9 @@
 const db = require("../models");
 
 module.exports = function(app) {
-  //get all the advertisements
+  //get all the advertisements includigng the business related to
   app.get("/api/advertisements", (req, res) => {
-    db.Advertisement.findAll({}).then(ads => {
+    db.Advertisement.findAll({ include: [db.Business] }).then(ads => {
       console.log(ads);
       res.json(ads);
     });
